2f34fafc2623e2c9a14018bfb1c94a2e3ae09931
Simplicio Carcamo <siarcarse@gmail.com>
2016-04-27 19:50:03 -0300

    triage

---
+++ js/data.js
@@ -0,0 +1,52 @@
+var slots = {
+    pane: [
+        'Abdominal Adultos',
+        'Abdominal Niños',
+        'Cervical',
+        'Cabeza',
+        'Espalda',
+        'Garganta',
+        'Testicular',
+        'Torácico'
+    ],
+    adult: [
+        'Síncope/Lipotimia',
+        'Ebrio',
+        'Comportamiento Extraño',
+        'Convulsiones',
+        'Enfermedad Mental',
+        'Sobredosis/Envenenamiento',
+    ],
+    trauma: [
+        'Agresión',
+        'Caídas',
+        'Lesión en tronco',
+        'Lesión en cabeza',
+        'Heridas',
+        'Lesión auto-inferida',
+        'Mordedura/Picadura',
+        'Quemadura/Erosión',
+        'Politraumatismo',
+    ],
+    kid: [
+        'Bebe Llora',
+        'Dolor Abdominal',
+        'Dificultad Respatoria',
+        'Niño Cojeando',
+        'Mal Estado General',
+        'Irritable',
+        'Padres Preocupados',
+    ],
+    asma: [
+
+    ],
+    diabetes: [
+
+    ],
+    pregnant: [
+
+    ],
+    hemorragia: [
+
+    ]
+};
---
+++ js/triage.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $.get('menuTop.html', function(data) {
+        $("#menuTop").html(data);
+    });
+    $('#back').click(function(event) {
+        $('#second-step').fadeOut(400, function() {
+            $('#first-step').fadeIn(400);
+        });
+    });
+    $('.first-step').click(function(event) {
+        if (this.id === 'asma' || this.id === 'diabetes' || this.id === 'pregnant' || this.id === 'hemorragia') {
+            var msg = "";
+            if (this.id === 'asma') msg = 'Paciente con asma';
+            if (this.id === 'diabetes') msg = 'Paciente con asma';
+            if (this.id === 'pregnant') msg = 'Paciente Embarazada';
+            if (this.id === 'hemorragia') msg = 'Paciente con hemorragia';
+            alert(msg);
+        } else {
+            var paneSlot = generateSlot(this.id);
+            $('#first-step').fadeOut(400, function() {
+                $('#containerStep').html(paneSlot);
+                $('#second-step').fadeIn(400);
+            });
+        }
+
+    });
+});
+
+function generateSlot(typeSlot) {
+    var html = '';
+    for (var i = 0; i < slots[typeSlot].length; i++) {
+        html += '<div class="col s12 m4 waves-effect waves-light">';
+        html += '<div id="pane" class="first-step shadow-demo valign-wrapper hoverable">';
+        html += '<h5 class="valign center truncate">' + slots[typeSlot][i] + '</h5>';
+        html += '</div>';
+        html += '</div>';
+    }
+    return html;
+}
